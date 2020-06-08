51fd26be4e2fe23b10192821d89b50ac571c2e0b
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-08 12:53:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ def using_concat(array1, array2)
   array1.concat(["sports cars", "flatiron school"])
 end
 
-array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+array = [1, 2, 3, 4]
 def using_insert(array, element)
   array.insert(element)
 end
